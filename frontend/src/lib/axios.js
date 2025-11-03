22ca74e17a258d3ad5e12cd6b9b74192b35a0038
sirajshaikh-dev <ss1425306@gmail.com>
2025-11-04 04:10:40 +0530

    add backend base url to frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
+import dotenv from 'dotenv'
 
+dotenv.config()
+const baseURL = import.meta.env.VITE_BASE_URL;
+console.log(baseURL);
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: `${baseURL}/api/v1`,
   withCredentials: true,
 });
