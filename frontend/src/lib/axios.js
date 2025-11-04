4c4e576da2ea846dea7feced5bc07e4823e95a0d
sirajshaikh-dev <ss1425306@gmail.com>
2025-11-04 19:56:20 +0530

    add backend url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,6 @@
 import axios from "axios";
-import dotenv from 'dotenv'
 
-dotenv.config()
-const baseURL = import.meta.env.VITE_BASE_URL;
-console.log(baseURL);
 export const axiosInstance = axios.create({
-  baseURL: `${baseURL}/api/v1`,
+  baseURL: ["http://localhost:8080/api/v1", "https://leetlab-h95h.onrender.com/api/v1"],
   withCredentials: true,
 });
